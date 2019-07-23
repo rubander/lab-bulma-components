b4a3784d1aa0d680eff519f606797b877a47fa0e
rubander <ralexanderv89@gmail.com>
2019-07-23 14:49:52 +0200

    done

--- starter-code/src/components/Button.js
+++ starter-code/src/components/Button.js
@@ -5,7 +5,7 @@ export default class Button extends Component {
     return (
       <div>
         <button class="button is-rounded my-class is-danger is-small pad">
-Login        </button>
+Login  </button>
         <button class="button is-small is-success">Sign Up</button>
       </div>
     );
